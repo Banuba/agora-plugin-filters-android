4a90e7cb804043f29ae0d09ac3ef4ce1bf4bd92d
romanbylina <roman.bylina@banuba.com>
2022-07-18 16:03:12 +0300

    fix error and update oep

--- banuba-agora-plugin/src/main/plugin_sources/effect_player.cpp
+++ banuba-agora-plugin/src/main/plugin_sources/effect_player.cpp
@@ -1,15 +1,206 @@
 #include "effect_player.hpp"
 
+#include <iostream>
 #include <thread>
 #include <optional>
 #include <iostream>
 
-#include <bnb/recognizer/interfaces/utility_manager.hpp>
-
-namespace
+namespace bnb::oep
 {
+
+    class js_callback : public bnb::interfaces::js_callback
+    {
+    public:
+        js_callback(oep_eval_js_result_cb callback)
+                : m_callback(std::move(callback)){};
+
+        void on_result(const std::string& result) override
+        {
+            m_callback(result);
+        }
+
+    private:
+        oep_eval_js_result_cb m_callback;
+    }; /* class js_callback */
+
+} /* namespace bnb::oep */
+
+namespace bnb::oep
+{
+
+    /* effect_player::create */
+    effect_player_sptr interfaces::effect_player::create(int32_t width, int32_t height)
+    {
+        // This particular example relies on OpenGL, so it should be explicitly requested
+        bnb::interfaces::effect_player::set_render_backend(::bnb::interfaces::render_backend_type::opengl);
+
+        return std::make_shared<bnb::oep::effect_player>(width, height);
+    }
+
+    // the description of the passed parameters to the Banuba SDK effext_player can be found at the link:
+    // https://docs.banuba.com/face-ar-sdk/generated/doxygen/html/structbnb_1_1interfaces_1_1effect__player__configuration.html#a810709129e2bc13eae190305861345ce
+    // NOTE: The parameters fx_width and fx height explicitly influence performance,
+    // for instance, if you have a small screen, e.g. 6 inches, and your rendering surface
+    // is large, e.g. 4K, then it is not necessary to render effect in 4K resolution
+    // since such precision will not be seen on the screen, so the performance can be improved
+    // via rendering the effect on a smaller surface.
+    // In the sample effect frame buffer and the surface are synced in surface_created and surface_changed methods.
+
+    /* effect_player::effect_player CONSTRUCTOR */
+    effect_player::effect_player(int32_t width, int32_t height)
+            : m_ep(bnb::interfaces::effect_player::create({
+                  width, // fx_width - the effect's framebuffer width
+                  height, // fx_height - the effect's framebuffer height
+                  bnb::interfaces::nn_mode::enable,
+                  bnb::interfaces::face_search_mode::good,
+                  false,
+                  false}))
+    {
+    }
+
+    /* effect_player::~effect_player */
+    effect_player::~effect_player() = default;
+
+    /* effect_player::surface_created */
+    void effect_player::surface_created(int32_t width, int32_t height)
+    {
+        m_ep->surface_created(width, height);
+        surface_changed(width, height);
+    }
+
+    /* effect_player::surface_changed */
+    void effect_player::surface_changed(int32_t width, int32_t height)
+    {
+        m_ep->surface_changed(width, height);
+        // Set explicitly the framebuffer of Effect Player to sync with surface size
+        if (auto em = m_ep->effect_manager()) {
+            em->set_effect_size(width, height);
+        }
+    }
+
+    /* effect_player::surface_destroyed */
+    void effect_player::surface_destroyed()
+    {
+        m_ep->surface_destroyed();
+    }
+
+    /* effect_player::load_effect */
+    bool effect_player::load_effect(const std::string& effect)
+    {
+        if (auto effect_manager = m_ep->effect_manager()) {
+            effect_manager->load(effect);
+            return true;
+        }
+        return false;
+    }
+
+    /* effect_player::call_js_method */
+    bool effect_player::call_js_method(const std::string& method, const std::string& param)
+    {
+        if (auto e_manager = m_ep->effect_manager()) {
+            if (auto effect = e_manager->current()) {
+                effect->call_js_method(method, param);
+            } else {
+                std::cout << "[Error] effect not loaded" << std::endl;
+                return false;
+            }
+        } else {
+            std::cout << "[Error] effect manager not initialized" << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    /* effect_player::eval_js */
+    void effect_player::eval_js(const std::string& script, oep_eval_js_result_cb result_callback)
+    {
+        if (auto e_manager = m_ep->effect_manager()) {
+            if (auto effect = e_manager->current()) {
+                std::shared_ptr<bnb::oep::js_callback> callback
+                        = result_callback ? std::make_shared<bnb::oep::js_callback>(std::move(result_callback)) : nullptr;
+                effect->eval_js(script, callback);
+            } else {
+                std::cout << "[Error] effect not loaded" << std::endl;
+            }
+        } else {
+            std::cout << "[Error] effect manager not initialized" << std::endl;
+        }
+    }
+
+    /* effect_player::pause */
+    void effect_player::pause()
+    {
+        m_ep->playback_pause();
+    }
+
+    /* effect_player::resume */
+    void effect_player::resume()
+    {
+        m_ep->playback_play();
+    }
+
+    /* effect_player::stop */
+    void effect_player::stop()
+    {
+        m_ep->playback_stop();
+    }
+
+    /* effect_player::push_frame */
+    void effect_player::push_frame(pixel_buffer_sptr image, bnb::oep::interfaces::rotation image_orientation, bool require_mirroring)
+    {
+        using ns = bnb::oep::interfaces::image_format;
+        auto bnb_image_format = make_bnb_image_format(image, image_orientation, require_mirroring);
+        switch (image->get_image_format()) {
+            case ns::bpc8_rgb:
+            case ns::bpc8_bgr:
+            case ns::bpc8_rgba:
+            case ns::bpc8_bgra:
+            case ns::bpc8_argb:
+                m_ep->push_frame(
+                        full_image_t(bpc8_image_t(
+                                color_plane(image->get_base_sptr()),
+                                make_bnb_pixel_format(image),
+                                bnb_image_format)));
+                break;
+            case ns::nv12_bt601_full:
+            case ns::nv12_bt601_video:
+            case ns::nv12_bt709_full:
+            case ns::nv12_bt709_video:
+                m_ep->push_frame(
+                        full_image_t(yuv_image_t(
+                                color_plane(image->get_base_sptr_of_plane(0)),
+                                color_plane(image->get_base_sptr_of_plane(1)),
+                                bnb_image_format,
+                                make_bnb_yuv_format(image))));
+                break;
+            case ns::i420_bt601_full:
+            case ns::i420_bt601_video:
+            case ns::i420_bt709_full:
+            case ns::i420_bt709_video:
+                m_ep->push_frame(
+                        full_image_t(yuv_image_t(
+                                color_plane(image->get_base_sptr_of_plane(0)),
+                                color_plane(image->get_base_sptr_of_plane(1)),
+                                color_plane(image->get_base_sptr_of_plane(2)),
+                                bnb_image_format,
+                                make_bnb_yuv_format(image))));
+                break;
+            default:
+                break;
+        }
+    }
+
+    /* effect_player::draw */
+    void effect_player::draw()
+    {
+        while (m_ep->draw() < 0) {
+            std::this_thread::yield();
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }
+
     /* effect_player::make_bnb_image_format */
-    bnb::image_format make_bnb_image_format(const pixel_buffer_sptr& image, bnb::oep::interfaces::rotation orientation)
+    bnb::image_format effect_player::make_bnb_image_format(pixel_buffer_sptr image, interfaces::rotation orientation, bool require_mirroring)
     {
         bnb::camera_orientation camera_orient {bnb::camera_orientation::deg_0};
 
@@ -23,16 +214,17 @@ namespace
             case ns::deg180:
                 camera_orient = bnb::camera_orientation::deg_180;
                 break;
+                break;
             case ns::deg270:
                 camera_orient = bnb::camera_orientation::deg_270;
                 break;
         }
 
-        return {static_cast<uint32_t>(image->get_width()), static_cast<uint32_t>(image->get_height()), camera_orient, false, 0, std::nullopt};
+        return {static_cast<uint32_t>(image->get_width()), static_cast<uint32_t>(image->get_height()), camera_orient, require_mirroring, 0, std::nullopt};
     }
 
     /* effect_player::make_bnb_yuv_format */
-    bnb::yuv_format_t make_bnb_yuv_format(const pixel_buffer_sptr& image)
+    bnb::yuv_format_t effect_player::make_bnb_yuv_format(pixel_buffer_sptr image)
     {
         bnb::yuv_format format {bnb::yuv_format::yuv_nv12};  /* i.e. NV12 or I420 */
         bnb::color_std standard {bnb::color_std::bt601}; /* i.e. BT.601 or BT.709 */
@@ -76,7 +268,7 @@ namespace
     }
 
     /* effect_player::make_bnb_pixel_format */
-    bnb::interfaces::pixel_format make_bnb_pixel_format(const pixel_buffer_sptr& image)
+    bnb::interfaces::pixel_format effect_player::make_bnb_pixel_format(pixel_buffer_sptr image)
     {
         bnb::interfaces::pixel_format fmt {bnb::interfaces::pixel_format::rgb};
 
@@ -100,156 +292,6 @@ namespace
                 break;
         }
         return fmt;
-    }
-
-}
-
-namespace bnb::oep
-{
-    effect_player::sdk_initializer::sdk_initializer(const std::vector<std::string>& paths, const std::string& client_token)
-    {
-        bnb::interfaces::utility_manager::initialize(paths, client_token);
-    }
-
-    effect_player::sdk_initializer::~sdk_initializer()
-    {
-        bnb::interfaces::utility_manager::release();
-    }
-
-    /* effect_player::create */
-    effect_player_sptr interfaces::effect_player::create(const std::vector<std::string>& path_to_resources, const std::string& client_token)
-    {
-        return std::make_shared<bnb::oep::effect_player>(path_to_resources, client_token);
-    }
-
-    /* effect_player::effect_player CONSTRUCTOR */
-    effect_player::effect_player(const std::vector<std::string>& path_to_resources, const std::string& client_token)
-        : m_utility(path_to_resources, client_token)
-        // the description of the passed parameters can be found at the link:
-        // https://docs.banuba.com/face-ar-sdk/generated/doxygen/html/structbnb_1_1interfaces_1_1effect__player__configuration.html#a810709129e2bc13eae190305861345ce
-        , m_ep(bnb::interfaces::effect_player::create({
-            1,
-            1,
-            bnb::interfaces::nn_mode::automatically,
-            bnb::interfaces::face_search_mode::good,
-            false,
-            false}))
-    {
-    }
-
-    /* effect_player::~effect_player */
-    effect_player::~effect_player() = default;
-
-    /* effect_player::surface_created */
-    void effect_player::surface_created(int32_t width, int32_t height)
-    {
-        m_ep->surface_created(width, height);
-    }
-
-    /* effect_player::surface_changed */
-    void effect_player::surface_changed(int32_t width, int32_t height)
-    {
-        m_ep->surface_changed(width, height);
-        m_ep->effect_manager()->set_effect_size(width, height);
-    }
-
-    /* effect_player::surface_destroyed */
-    void effect_player::surface_destroyed()
-    {
-        m_ep->surface_destroyed();
-    }
-
-    /* effect_player::load_effect */
-    bool effect_player::load_effect(const std::string& effect)
-    {
-        if (auto effect_manager = m_ep->effect_manager()) {
-            effect_manager->load(effect);
-            return true;
-        }
-        return false;
-    }
-
-    /* effect_player::call_js_method */
-    bool effect_player::call_js_method(const std::string& method, const std::string& param)
-    {
-        if (auto e_manager = m_ep->effect_manager()) {
-            if (auto effect = e_manager->current()) {
-                effect->call_js_method(method, param);
-            } else {
-                std::cout << "[Error] effect not loaded" << std::endl;
-                return false;
-            }
-        } else {
-            std::cout << "[Error] effect manager not initialized" << std::endl;
-            return false;
-        }
-        return true;
-    }
-
-    /* effect_player::pause */
-    void effect_player::pause()
-    {
-        m_ep->playback_pause();
-    }
-
-    /* effect_player::resume */
-    void effect_player::resume()
-    {
-        m_ep->playback_play();
-    }
-
-    /* effect_player::push_frame */
-    void effect_player::push_frame(pixel_buffer_sptr image, bnb::oep::interfaces::rotation image_orientation)
-    {
-        using ns = bnb::oep::interfaces::image_format;
-        auto bnb_image_format = make_bnb_image_format(image, image_orientation);
-        switch (image->get_image_format()) {
-            case ns::bpc8_rgb:
-            case ns::bpc8_bgr:
-            case ns::bpc8_rgba:
-            case ns::bpc8_bgra:
-            case ns::bpc8_argb:
-                m_ep->push_frame(
-                    full_image_t(bpc8_image_t(
-                        color_plane(image->get_base_sptr()),
-                        make_bnb_pixel_format(image),
-                        bnb_image_format)));
-                break;
-            case ns::nv12_bt601_full:
-            case ns::nv12_bt601_video:
-            case ns::nv12_bt709_full:
-            case ns::nv12_bt709_video:
-                m_ep->push_frame(
-                    full_image_t(yuv_image_t(
-                        color_plane(image->get_base_sptr_of_plane(0)),
-                        color_plane(image->get_base_sptr_of_plane(1)),
-                        bnb_image_format,
-                        make_bnb_yuv_format(image))));
-                break;
-            case ns::i420_bt601_full:
-            case ns::i420_bt601_video:
-            case ns::i420_bt709_full:
-            case ns::i420_bt709_video:
-                m_ep->push_frame(
-                    full_image_t(yuv_image_t(
-                        color_plane(image->get_base_sptr_of_plane(0)),
-                        color_plane(image->get_base_sptr_of_plane(1)),
-                        color_plane(image->get_base_sptr_of_plane(2)),
-                        bnb_image_format,
-                        make_bnb_yuv_format(image))));
-                break;
-            default:
-                break;
-        }
-    }
-
-    /* effect_player::draw */
-    void effect_player::draw()
-    {
-        while (m_ep->draw() < 0) {
-            std::this_thread::yield();
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
     }
 
 } /* namespace bnb::oep */
--- banuba-agora-plugin/src/main/plugin_sources/effect_player.hpp
+++ banuba-agora-plugin/src/main/plugin_sources/effect_player.hpp
@@ -9,36 +9,39 @@ namespace bnb::oep
     class effect_player : public bnb::oep::interfaces::effect_player
     {
     public:
-        effect_player(const std::vector<std::string>& path_to_resources, const std::string& client_token);
+        effect_player(int32_t width, int32_t height);
+
         ~effect_player();
 
         void surface_created(int32_t width, int32_t height) override;
+
         void surface_changed(int32_t width, int32_t height) override;
+
         void surface_destroyed() override;
 
         bool load_effect(const std::string& effect) override;
 
         bool call_js_method(const std::string& method, const std::string& param) override;
 
+        void eval_js(const std::string& script, oep_eval_js_result_cb result_callback) override;
+
         void pause() override;
+
         void resume() override;
 
-        void push_frame(pixel_buffer_sptr image, bnb::oep::interfaces::rotation image_orientation) override;
+        void stop() override;
+
+        void push_frame(pixel_buffer_sptr image, bnb::oep::interfaces::rotation image_orientation, bool require_mirroring) override;
 
         void draw() override;
 
     private:
-        // temp drop-in replacement for bnb::utility due to symbol export issues
-        // TODO: drop it completely and use just utility_manager instead
-        class sdk_initializer
-        {
-        public:
-            sdk_initializer(const std::vector<std::string>& paths, const std::string& client_token);
-            ~sdk_initializer();
-        };
-        sdk_initializer m_utility;
+        bnb::image_format make_bnb_image_format(pixel_buffer_sptr image, interfaces::rotation orientation, bool require_mirroring);
+        bnb::yuv_format_t make_bnb_yuv_format(pixel_buffer_sptr image);
+        bnb::interfaces::pixel_format make_bnb_pixel_format(pixel_buffer_sptr image);
+
+    private:
         std::shared_ptr<bnb::interfaces::effect_player> m_ep;
-        std::atomic_bool m_is_surface_created {false};
     }; /* class effect_player */
 
 } /* namespace bnb::oep */
